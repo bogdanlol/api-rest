be1cdb05672d76018b92948491c35529c4f6a35b
ys50wd <bogdan-marian.luca@ing.com>
2021-05-19 17:30:36 +0300

    feat(cp4d):add demo

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/TutorialEdge/create-rest-api-in-go-tutorial
 
 go 1.12
 
-require github.com/gorilla/mux v1.7.2
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/gorilla/mux v1.7.2
+)
--- main.go
+++ main.go
@@ -1,89 +1,94 @@
-
 // main.go
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "log"
-    "io/ioutil"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 
-    "github.com/gorilla/mux"
+	"github.com/davecgh/go-spew/spew"
+
+	"github.com/gorilla/mux"
 )
 
 // Article - Our struct for all articles
-type Article struct {
-    Id      string    `json:"Id"`
-    Title   string `json:"Title"`
-    Desc    string `json:"desc"`
-    Content string `json:"content"`
+
+type Config struct {
+	Class  string `json:"class"`
+	Tasks  string `json:"tasks"`
+	Topics string `json:"topics"`
+}
+type Connector struct {
+	Id     string `json:"id"`
+	Name   string `json:"name"`
+	Config Config `json:"config"`
 }
 
-var Articles []Article
+var connectors []Connector
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
 }
 
-func returnAllArticles(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("Endpoint Hit: returnAllArticles")
-    json.NewEncoder(w).Encode(Articles)
+func returnAllConnectors(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	spew.Dump(connectors)
+	json.NewEncoder(w).Encode(connectors)
 }
 
-func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    key := vars["id"]
+func returnSingleConenctor(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
 
-    for _, article := range Articles {
-        if article.Id == key {
-            json.NewEncoder(w).Encode(article)
-        }
-    }
+	for _, connector := range connectors {
+		if connector.Id == key {
+			json.NewEncoder(w).Encode(connector)
+		}
+	}
 }
 
+func createNewConnector(w http.ResponseWriter, r *http.Request) {
+	// get the body of our POST request
+	// unmarshal this into a new Article struct
+	// append this to our Articles array.
+	reqBody, _ := ioutil.ReadAll(r.Body)
 
-func createNewArticle(w http.ResponseWriter, r *http.Request) {
-    // get the body of our POST request
-    // unmarshal this into a new Article struct
-    // append this to our Articles array.    
-    reqBody, _ := ioutil.ReadAll(r.Body)
-    var article Article 
-    json.Unmarshal(reqBody, &article)
-    // update our global Articles array to include
-    // our new Article
-    Articles = append(Articles, article)
+	var connector Connector
+	json.Unmarshal(reqBody, &connector)
+	// // update our global Articles array to include
+	// // our new Article
+	spew.Dump(connector)
+	connectors = append(connectors, connector)
 
-    json.NewEncoder(w).Encode(article)
+	json.NewEncoder(w).Encode(connectors)
 }
 
-func deleteArticle(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    id := vars["id"]
+func deleteConnector(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
 
-    for index, article := range Articles {
-        if article.Id == id {
-            Articles = append(Articles[:index], Articles[index+1:]...)
-        }
-    }
+	for index, connector := range connectors {
+		if connector.Id == id {
+			connectors = append(connectors[:index], connectors[index+1:]...)
+		}
+	}
 
 }
 
 func handleRequests() {
-    myRouter := mux.NewRouter().StrictSlash(true)
-    myRouter.HandleFunc("/", homePage)
-    myRouter.HandleFunc("/articles", returnAllArticles)
-    myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
-    myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
-    myRouter.HandleFunc("/article/{id}", returnSingleArticle)
-    log.Fatal(http.ListenAndServe(":10000", myRouter))
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/connectors", returnAllConnectors)
+	myRouter.HandleFunc("/connector", createNewConnector).Methods("POST")
+	myRouter.HandleFunc("/connector/{id}", deleteConnector).Methods("DELETE")
+	myRouter.HandleFunc("/connector/{id}", returnSingleConenctor)
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 
 func main() {
-    Articles = []Article{
-        Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-        Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
-    }
-    handleRequests()
+
+	handleRequests()
 }
