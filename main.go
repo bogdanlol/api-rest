c9436a36a81cea21e9564b270defa644099ec140
ys50wd <bogdan-marian.luca@ing.com>
2021-05-20 12:37:06 +0300

    fix(main): main.go

--- main.go
+++ main.go
@@ -2,131 +2,130 @@
 package main
 
 import (
-                "bytes"
-                "encoding/json"
-                "fmt"
-                "io/ioutil"
-                "log"
-                "net/http"
-                "strings"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 
-                "github.com/davecgh/go-spew/spew"
+	"github.com/davecgh/go-spew/spew"
 
-                "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
 // Article - Our struct for all articles
 
 type Config struct {
-                Class  string `json:"connector.class"`
-                Tasks  string `json:"tasks.max"`
-                Topics string `json:"topics"`
-                File   string `json:"file"`
-                FilePattern string `json:"file.pattern"`
-                Converter string `json:"value.converter"`
-                SchemaRegistry string `json:"value.converter.schema.registry.url"`
-
+	Class          string `json:"connector.class"`
+	Tasks          string `json:"tasks.max"`
+	Topics         string `json:"topics"`
+	File           string `json:"file"`
+	FilePattern    string `json:"file.pattern"`
+	Converter      string `json:"value.converter"`
+	SchemaRegistry string `json:"value.converter.schema.registry.url"`
 }
 type Connector struct {
-                Id     string `json:"id"`
-                Name   string `json:"name"`
-                Config Config `json:"config"`
+	Id     string `json:"id"`
+	Name   string `json:"name"`
+	Config Config `json:"config"`
 }
 
 var connectors []Connector
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-                fmt.Fprintf(w, "Api Gateway For Cp4D F055 Streaming!")
-                fmt.Println("Endpoint Hit: homePage")
+	fmt.Fprintf(w, "Api Gateway For Cp4D F055 Streaming!")
+	fmt.Println("Endpoint Hit: homePage")
 }
 
 func returnAllConnectors(w http.ResponseWriter, r *http.Request) {
-                fmt.Println("Endpoint Hit: returnAllArticles")
-                spew.Dump(connectors)
-                json.NewEncoder(w).Encode(connectors)
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	spew.Dump(connectors)
+	json.NewEncoder(w).Encode(connectors)
 }
 
 func returnSingleConenctor(w http.ResponseWriter, r *http.Request) {
-                vars := mux.Vars(r)
-                key := vars["id"]
+	vars := mux.Vars(r)
+	key := vars["id"]
 
-                for _, connector := range connectors {
-                                if connector.Id == key {
-                                                json.NewEncoder(w).Encode(connector)
-                                }
-                }
+	for _, connector := range connectors {
+		if connector.Id == key {
+			json.NewEncoder(w).Encode(connector)
+		}
+	}
 }
 
 func createNewConnector(w http.ResponseWriter, r *http.Request) {
-                // get the body of our POST request
-                // unmarshal this into a new Article struct
-                // append this to our Articles array.
-                reqBody, _ := ioutil.ReadAll(r.Body)
+	// get the body of our POST request
+	// unmarshal this into a new Article struct
+	// append this to our Articles array.
+	reqBody, _ := ioutil.ReadAll(r.Body)
 
-                var connector Connector
-                json.Unmarshal(reqBody, &connector)
-                // // update our global Articles array to include
-                // // our new Article
-                spew.Dump(connector)
+	var connector Connector
+	json.Unmarshal(reqBody, &connector)
+	// // update our global Articles array to include
+	// // our new Article
+	spew.Dump(connector)
 
-                // sanitize connector
-                if connector.Config.Class == "" {
-                                panic("ERROR: Connector class not specified!")
-                }
-                if connector.Config.Topics == "" {
-                                panic("ERROR: Missing topic information!")
-                }
-                if connector.Config.Converter == "" {
-                                panic("ERROR: Missing Converter information!")
-                }
-                if connector.Config.SchemaRegistry == "" {
-                                panic("ERROR: Missing SchemaRegistry information!")
-                }
-                if strings.Contains(connector.Config.Class, "FileStreamSinkConnector") {
-                                if connector.Config.File == "" {
-                                                panic("ERROR: Output file not specified")
-                                }
-                                if connector.Config.FilePattern == "" {
-                                                connector.Config.FilePattern = "'.'yyyy-MM-dd-HH-mm"
-                                }
-                }
+	// sanitize connector
+	if connector.Config.Class == "" {
+		panic("ERROR: Connector class not specified!")
+	}
+	if connector.Config.Topics == "" {
+		panic("ERROR: Missing topic information!")
+	}
+	if connector.Config.Converter == "" {
+		panic("ERROR: Missing Converter information!")
+	}
+	if connector.Config.SchemaRegistry == "" {
+		panic("ERROR: Missing SchemaRegistry information!")
+	}
+	if strings.Contains(connector.Config.Class, "FileStreamSinkConnector") {
+		if connector.Config.File == "" {
+			panic("ERROR: Output file not specified")
+		}
+		if connector.Config.FilePattern == "" {
+			connector.Config.FilePattern = "'.'yyyy-MM-dd-HH-mm"
+		}
+	}
 
-                jsonValue, _ := json.Marshal(connector)
-                spew.Dump(jsonValue)
-                resp, err := http.Post("http://localhost:8081/connectors", "application/json", bytes.NewBuffer(jsonValue))
+	jsonValue, _ := json.Marshal(connector)
+	spew.Dump(jsonValue)
+	resp, err := http.Post("http://localhost:8081/connectors", "application/json", bytes.NewBuffer(jsonValue))
 
-                if err != nil {
-                                panic(err)
-                }
+	if err != nil {
+		panic(err)
+	}
 
-                fmt.Println(resp.Status)
-                json.NewEncoder(w).Encode(connectors)
+	fmt.Println(resp.Status)
+	json.NewEncoder(w).Encode(connectors)
 
 }
 
 func deleteConnector(w http.ResponseWriter, r *http.Request) {
-                vars := mux.Vars(r)
-                id := vars["id"]
+	vars := mux.Vars(r)
+	id := vars["id"]
 
-                for index, connector := range connectors {
-                                if connector.Id == id {
-                                                connectors = append(connectors[:index], connectors[index+1:]...)
-                                }
-                }
+	for index, connector := range connectors {
+		if connector.Id == id {
+			connectors = append(connectors[:index], connectors[index+1:]...)
+		}
+	}
 
 }
 
 func handleRequests() {
-                myRouter := mux.NewRouter().StrictSlash(true)
-                myRouter.HandleFunc("/", homePage)
-                myRouter.HandleFunc("/connectors", returnAllConnectors)
-                myRouter.HandleFunc("/connector", createNewConnector).Methods("POST")
-                myRouter.HandleFunc("/connector/{id}", deleteConnector).Methods("DELETE")
-                myRouter.HandleFunc("/connector/{id}", returnSingleConenctor)
-                log.Fatal(http.ListenAndServe(":8080", myRouter))
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/connectors", returnAllConnectors)
+	myRouter.HandleFunc("/connector", createNewConnector).Methods("POST")
+	myRouter.HandleFunc("/connector/{id}", deleteConnector).Methods("DELETE")
+	myRouter.HandleFunc("/connector/{id}", returnSingleConenctor)
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 
 func main() {
 
-                handleRequests()
+	handleRequests()
 }
